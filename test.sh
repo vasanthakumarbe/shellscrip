6bd131615b0598906296c0db9af5c1b24808f6b0
vasanthakumarbe <110677150+vasanthakumarbe@users.noreply.github.com>
2022-09-16 12:31:40 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,13 +9,10 @@ now=$(date)
 echo "$now"
 echo "Current date: $now"
 date +'FORMAT'
- 
 ### mm/dd/yyyy ###
 date +'%m/%d/%Y'
 now="$(date)"
 printf "Current date and time %s\n" "$now"
- 
 now="$(date +'%d/%m/%Y')"
 printf "Current date in dd/mm/yyyy format %s\n" "$now"
- 
 echo "Starting backup at $now, please wait..."
