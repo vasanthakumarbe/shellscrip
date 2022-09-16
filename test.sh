26874e4f522cb7c575a6a1646e504a8380675c5f
vasanthakumarbe <110677150+vasanthakumarbe@users.noreply.github.com>
2022-09-16 22:10:38 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,20 @@ ls
 pwd
 timeStamp=$(date +%s)
 echo "###### TIMESTAMP IS #$timeStamp #######"
-
-
+var=$(date)
+var=`date`
+echo "$var"
+now=$(date)
+echo "$now"
+echo "Current date: $now"
+date +'FORMAT'
+ 
+### mm/dd/yyyy ###
+date +'%m/%d/%Y'
+now="$(date)"
+printf "Current date and time %s\n" "$now"
+ 
+now="$(date +'%d/%m/%Y')"
+printf "Current date in dd/mm/yyyy format %s\n" "$now"
+ 
+echo "Starting backup at $now, please wait..."
