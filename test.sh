f55db2c9c4e2e05f07c4935c00d86a051cd50498
vasanthakumarbe <110677150+vasanthakumarbe@users.noreply.github.com>
2022-09-21 10:12:46 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
-pwd
-ls -lrt
-date -d "$(date +"%Y-%m-01") - 1 day" +"%F"
+#!/bin/bash
+for monthsback in 1 2 3 4 5 6 7 8 9 10 11 12
+do
+    monthsfwd=`expr $monthsback - 1`
+    startdate=`date -d "-${monthsback} month -$(($(date +%d)-1)) days" +%Y-%m-%d`
+    enddate=`date -d "-$(date +%d) days -${monthsfwd} month" +%Y-%m-%d`
+    echo "$monthsback month(s) ago:"
+    echo $startdate
+    echo $enddate
+    echo ""
+done
