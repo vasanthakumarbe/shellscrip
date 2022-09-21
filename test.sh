b4b94a18044e243a215467722523bddc2a01d2cc
vasanthakumarbe <110677150+vasanthakumarbe@users.noreply.github.com>
2022-09-21 08:48:49 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 pwd
 ls -lrt
-$ date -d "$(date +%Y/%m/01) - 1 day" "+%Y/%m/%d"
+set -A months Dec Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov
+x='date +%m'
+y='expr $x -1'
+year='date +%y'
+month='perl -MPOSIX=strftime -le '@t = localtime; $t[4]--;print strftime("%m",@t)'
+month_abv= 'echo ${months[$y]}'
+
